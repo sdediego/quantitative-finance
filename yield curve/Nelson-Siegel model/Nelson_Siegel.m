9b766dde534ec8c477765b1201a6e462e0883a02
Sergio de Diego <sdediego@users.noreply.github.com>
2017-09-25 13:53:43 +0200

    Add files via upload

--- yield curve/Nelson-Siegel model/Nelson_Siegel.m
+++ yield curve/Nelson-Siegel model/Nelson_Siegel.m
@@ -1,1 +1,21 @@
-
+function  r = Nelson_Siegel(t,b0,b1,b2,tau)
+% Nelson_Siegel: Nelson-Siegel model for term structure of interests rates.
+%
+% SINTAXIS:  
+%  r = Nelson_Siegel(t,b0,b1,b2,tau)
+%
+%    t : Time
+%  tau : Decay factor
+%   b0 : Long run levels of interest rates
+%   b1 : Short-term component of interest rates
+%   b2 : Medium-term component of interest rates
+%
+% EXAMPLE: 
+%  tau = 0.5; b0 = 4; b1 = -2; b2 = -6;
+%  nPlot = 1000;  tPlot = linspace(0,3,nPlot);
+%  rPlot = Nelson_Siegel(tPlot,b0,b1,b2,tau);
+%  figure(1); plot(tPlot, rPlot);
+%  xlabel('t (years)'); ylabel('interest rate(%)')
+%  
+r = b0 + b1*exp(-t/tau) + b2*(t/tau).*exp(-t/tau);
+end
