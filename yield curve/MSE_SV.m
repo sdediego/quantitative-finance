4b97771ff0117b47d6be0d554cf3a78fec41e486
Sergio de Diego <sdediego@users.noreply.github.com>
2017-09-26 18:08:59 +0200

    Delete MSE_SV.m

--- yield curve/MSE_SV.m
+++
@@ -1,6 +0,0 @@
-function MSE = MSE_SV(parameters,t,r)
-% MSE_SV: Mean Squared Error for the Svensson model.
-%
-parameters = num2cell(parameters);
-MSE = mean((Svensson(t, parameters{:})-r).^2);
-end
