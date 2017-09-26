2e4fc4d75a77cde363a15bb65beeeeff94ef970a
Sergio de Diego <sdediego@users.noreply.github.com>
2017-09-26 18:08:47 +0200

    Delete MSE_NS.m

--- yield curve/MSE_NS.m
+++
@@ -1,6 +0,0 @@
-function MSE = MSE_NS(parameters,t,r)
-% MSE_NS: Mean Squared Error for the Nelson-Siegel model.
-%
-parameters = num2cell(parameters); 
-MSE = mean((r - Nelson_Siegel(t, parameters{:})).^2);
-end
